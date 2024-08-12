0af8bc2d65cfe9d703a85d1f55bbd467843ff2b2
Aman Rawal <142312693+junker149@users.noreply.github.com>
2024-08-12 21:25:34 +0530

    Update Principal.js
    Added routing for Principal users

--- BACKEND/routes/Principal.js
+++ BACKEND/routes/Principal.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const Principal = express.Router();
 
+Principal.use(express.json());
+
+Principal.post("/login", (req, res) => {
+    const body = req.body;
+})
 module.exports=Principal;
