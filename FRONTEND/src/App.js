54b5b2b8171e09eb02cfff762b0a86cef288b42a
Aman Rawal <142312693+junker149@users.noreply.github.com>
2024-08-13 11:20:20 +0530

    Update App.js
    Added saving data functionality

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'React';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <>
     <div className='wrapper'>
     <form action="">
       <h1>LOGIN ID</h1>
       <div classroom="input-box">
-        <input type ="text" placeholder ='USERNAME' required/>
+        <input onchange=((e) =>{
+          setUsername(e.target.value);
+        }) type ="text" placeholder ='USERNAME' required/>
       </div>
       <div classroom="input-box">
-        <input type ="text" placeholder ='PASSWORD' required/>
+        <input onchange=((e) => {
+          setPassword(e.target.value);
+        }) type ="text" placeholder ='PASSWORD' required/>
       </div>
       <div className='remember-forget'>
         <label><input type="checkbox" />Remember me </label>
